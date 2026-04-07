7a29e1526dbfa1bccb858256885bd25271709c09
Ionyt1234 <ionstreamerpro@gmail.com>
2026-04-07 07:21:24 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,9 @@
-const { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes } = require('discord.js');
-
-const token = "MTQ5MTA3MjYxNzYxNTE5NjMwMg.G9HSTh.WdNV19L7EuzL8t0PUdwTeGQjtcOtNn8e_8HUqw";
-const clientId = "1491072617615196302";
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName('tpa')
-    .setDescription('toilet paper awesome')
-    .toJSON()
-];
-
-const rest = new REST({ version: '10' }).setToken(token);
-
-(async () => {
-  await rest.put(
-    Routes.applicationCommands(clientId),
-    { body: commands }
-  );
-})();
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'tpa') {
-    await interaction.reply(`toilet paper awesome
-toilet paper awesome`);
-  }
+client.once('ready', () => {
+  console.log('Bot is online!');
 });
 
 client.login(process.env.DISCORD_TOKEN);
