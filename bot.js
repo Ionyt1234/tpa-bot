8dbbffe989c2dba6f3a72ff84db4298c22603b0b
Ionyt1234 <ionstreamerpro@gmail.com>
2026-04-07 07:24:07 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,18 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once('ready', () => {
-  console.log('Bot is online!');
+client.once("ready", () => {
+  console.log("TPA bot is online!");
+});
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "tpa") {
+    await interaction.reply(`toilet paper awesome
+toilet paper awesome`);
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
