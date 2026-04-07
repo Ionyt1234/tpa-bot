ab760ce1868905477501b5dbe7c444a96b154c37
Ionyt1234 <ionstreamerpro@gmail.com>
2026-04-07 07:26:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,28 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Commands we want
+const commands = [
+  new SlashCommandBuilder()
+    .setName("tpa")
+    .setDescription("toilet paper awesome")
+    .toJSON()
+];
+
+// Register slash commands instantly on startup
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID), // put your Application ID here in Render secrets
+      { body: commands }
+    );
+    console.log("Slash command registered!");
+  } catch (err) {
+    console.error("Failed to register commands:", err);
+  }
+})();
 
 client.once("ready", () => {
   console.log("TPA bot is online!");
